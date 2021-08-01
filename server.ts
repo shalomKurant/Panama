8bed83c45797a01b0a3331dd8385eb15a892a1c1
Shalom Kurank <shalom.kurant@gmail.com>
2021-08-01 22:29:32 +0300

    add basic client and server files

---
+++ server.ts
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api/hello', (req: any, res: any) => {
+  res.send({ express: 'Hello From Express' });
+});
+app.post('/api/world', (req: any, res: any) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('*', function(req: any, res: any) {
+  console.log("FROM SERVER");
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
